524b83fa666ba6c7c0d6c09c7012eed341be20f3
chernikovmaksym <chernikovmaksym@gmail.com>
2018-02-26 22:51:06 +0200

    Base configuration added

---
+++ contract/contract.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.16;
+
+contract owned {
+    address public owner;
+
+    function owned() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        if (msg.sender != owner) revert();
+        _;
+    }
+}
+
+
+contract voting is owned {
+
+}
